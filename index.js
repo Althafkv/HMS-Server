0690aa11e741f7b4abd7ef80cc848fe75e65a5e2
Althafkv <althafkvworks@gmail.com>
2023-05-29 17:39:24 +0530

    hms server

---
+++ index.js
@@ -0,0 +1,24 @@
+// import json-server
+const jsonServer = require('json-server')
+
+// create json server app
+const server = jsonServer.create()
+
+// set up route for db.json
+const router = jsonServer.router("db.json")
+
+// return a middleware used by JSON Server
+const middleware = jsonServer.defaults()
+
+// setup port number for server app
+const port = process.env.PORT || 3000
+
+// use router and middleware in server app
+server.use(middleware)
+server.use(router)
+
+// to listen the server app in port
+server.listen(port,()=>{
+    console.log(`Contact server app started at port ${port}`);
+})
+
